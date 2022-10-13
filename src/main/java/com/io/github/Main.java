b50c624e9ebb92c6eff7908407b3edd696304533
Revathi G <revathikrishna0323@gmail.com>
2022-10-13 13:51:26 +0530

    in the main() method, created objects of the class and called the method and reformatted the code

--- src/main/java/com/io/github/Main.java
+++ src/main/java/com/io/github/Main.java
@@ -2,6 +2,10 @@ package com.io.github;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GameConsole gameConsole1 = new Chess();
+        gameConsole1.computeScore();
+        GameConsole gameConsole2 = new Badminton();
+        gameConsole2.computeScore();
+
     }
 }
